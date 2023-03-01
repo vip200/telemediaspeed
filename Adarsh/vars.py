45682e8f66dfbfe196309ee1cbb7dcf39965e539
vip200 <51438486+vip200@users.noreply.github.com>
2023-03-01 21:45:34 +0200

    Add files via upload

---
+++ Adarsh/vars.py
@@ -0,0 +1,33 @@
+# (c) adarsh-goel
+
+from os import getenv, environ
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class Var(object):
+    API_ID = 4857965
+    API_HASH = "801abaf29775cf564efd759def571091"
+    BOT_TOKEN = "5010315993:AAG5RNiWdN4LepyTgP61DoReM89HoLyUCtg"
+    SESSION_NAME = str(getenv('SESSION_NAME', 'telemediaspeed'))
+    SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
+    WORKERS = int(getenv('WORKERS', '4'))
+    BIN_CHANNEL = -1001497492623
+    PORT = int(getenv('PORT', 8080))
+    BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', 'https://telemediaspeed.herokuapp.com'))
+    OWNER_ID = int(getenv('OWNER_ID', '384403734'))
+    NO_PORT = bool(getenv('NO_PORT', False))
+    APP_NAME = None
+    OWNER_USERNAME = 'https://t.me/xbmc19'
+    if 'DYNO' in environ:
+        ON_HEROKU = True
+        APP_NAME = 'telemediaspeed'
+    else:
+        ON_HEROKU = False
+    FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
+    URL = "https://{}/".format(FQDN) if ON_HEROKU or NO_PORT else \
+        "http://{}:{}/".format(FQDN, PORT)
+    DATABASE_URL = 'mongodb+srv://yossi7229:Cc200200!@cluster0.vvxkqcq.mongodb.net'#str(getenv('DATABASE_URL'))
+    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', None))
+    BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1001497492623")).split()))
